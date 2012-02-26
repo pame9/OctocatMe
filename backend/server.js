faa4750c9cc236398beed3db48d8668ec7ad634c
Sara Chipps <sarajchipps@Sara-Chippss-MacBook-Pro.local>
2012-02-25 20:57:36 -0500

    github auth

--- backend/server.js
+++ backend/server.js
@@ -7,11 +7,28 @@ var express = require('express')
     , mongoose = require('mongoose')
     , Schema = mongoose.Schema
     , io = require('socket.io')
+    , http = require('http')
+    , https = require('https')
     , routes = require('./routes/routes')
     , config = require('./lib/config')
     , everyauth = require('everyauth');
 
     
+
+  everyauth.github
+    .appId(config.gh_clientId)
+    .appSecret(config.gh_secret)
+    .findOrCreateUser( function (session, accessToken, accessTokenExtra, githubUserMetadata) {
+      session.oauth = accessToken;
+      return session.uid = githubUserMetadata.login;
+    })
+    .redirectPath('/');
+   everyauth.everymodule.handleLogout( function (req, res) {
+    req.logout();
+    req.session.uid = null;
+    res.writeHead(303, { 'Location': this.logoutRedirectPath() });
+    res.end();
+  });
 
 require('express-resource');
 
@@ -24,7 +41,6 @@ var app = module.exports = express.createServer(
   , express.static(__dirname + "/public")
   , express.cookieParser()
   , express.session({ secret: 'esoognom'})
-  , mongooseAuth.middleware()
 );
 
 
@@ -43,22 +59,41 @@ app.configure(function(){
   app.use(express.static(__dirname + '/public')); 
   app.set('views', __dirname + '/views');
   app.use(express.cookieParser());
-  app.use(express.session({store: sesh, secret: config.redis_secret}));
-  app.use(everyauth.middleware()); // pretend you didn't see this yet
-});
-
-app.get('/', function(req, res){
-  res.render("index");
+  app.use(everyauth.middleware()); 
 });
 
 app.get('/',function(req,res) {
 	if (req.session && req.session.uid) {
-	    return res.redirect('user/confirm');
+	    return res.redirect('/board');
 	}
-	res.render('user/login');
+	res.render('login');
 });
 
 
+
+app.get('/board',function(req,res) {
+    if (!req.session.uid) {
+        return res.redirect('/');
+    }
+    var repos,
+        opts = {
+			host: "api.github.com",
+			path: '/user/repos?access_token=' + req.session.oauth,
+			method: "GET"
+		},
+    	request = https.request(opts, function(resp) {
+    		var data = "";
+    		resp.setEncoding('utf8');
+		resp.on('data', function (chunk) {
+			data += chunk;
+		});
+		resp.on('end', function () {
+			repos = JSON.parse(data);
+			res.render('board',{username: req.session.uid, repos: repos});
+		});
+    	});
+    request.end();
+});
 
 app.configure('development', function(){
    app.use(express.errorHandler({ dumpExceptions: true }));
@@ -71,38 +106,13 @@ app.configure('production', function(){
   app.use(express.errorHandler()); 
 });
 
-
-
 // Routes
 
 // Setup the routes
 routes.init(app);
 
 var routes = require('./routes')
-app.get('/', routes.index);
 
-
-
-
-
-
-
-mongooseAuth.helpExpress(app);
 
 app.listen(3000);
 
-
-everyauth.github
-  .appId(config.gh_clientId)
-  .appSecret(config.gh_secret)
-  .findOrCreateUser( function (session, accessToken, accessTokenExtra, githubUserMetadata) {
-    session.oauth = accessToken;
-    return session.uid = githubUserMetadata.login;
-  })
-  .redirectPath('/');
- everyauth.everymodule.handleLogout( function (req, res) {
-  req.logout();
-  req.session.uid = null;
-  res.writeHead(303, { 'Location': this.logoutRedirectPath() });
-  res.end();
-});
